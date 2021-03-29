aab2ed54a43fc412dff81c63d10ad321fe4befa0
pkiop <pkiop@naver.com>
2021-03-29 23:10:08 +0900

    Feat: Add router, global style

---
+++ client/src/page/survey/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Survey() {
+  return (
+    <div className="App">
+      Survey
+    </div>
+  );
+}
+
+export default Survey;
---
+++ client/src/style/theme.ts
@@ -0,0 +1,32 @@
+const color = {
+  black: '#000000',
+  headerBar: '#222831',
+  mainColor: '#dddddd',
+  darkMainColor: '#aaaaaa',
+  subMainColor: '#222831',
+  white: '#FFFFFF',
+  navbar: '#222831',
+  timeInput: '#bbbbbb',
+  submit: '#bbbbbb',
+  submitHover: '#218838',
+  green: '#28a745',
+  delete: '#DC3545',
+  deleteHover: '#C82333',
+};
+
+const size = {
+  categoryLabelSize: {
+    width: '4.5rem',
+    height: '1.4rem',
+  },
+  mainInputRadius: '0.5rem',
+  headerBarHeight: '3rem',
+  navbarHeight: '3rem',
+};
+
+const theme = {
+  color,
+  size,
+};
+
+export default theme;
