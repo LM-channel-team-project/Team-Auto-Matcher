abeb235ad50b6de6bd4c91c86b75d8a0992a47e2
pkiop <pkiop@naver.com>
2021-04-03 23:14:02 +0900

    Feat: Add questionnaire in survay page

--- client/src/page/survey/index.tsx
+++ client/src/page/survey/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Questionnaire from 'component/orgamisms/Questionnaire';
 
 function Survey() {
+  const questionList = ['React', 'Vue', 'Vanila Javascript', 'React', 'Vue', 'Vanila Javascript'];
   return (
     <div className="App">
-      Survey
+      <Questionnaire question={'선호하는 기술스택을 고르세요'} questionList={questionList} bDuplicateSelect={false} />
     </div>
   );
 }
