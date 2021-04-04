8835af49684bfb027e979e821ee111a9d5ea3791
pkiop <pkiop@naver.com>
2021-04-04 16:39:50 +0900

    Feat: Add amplify for auth setting

--- client/src/Apollo.ts
+++ client/src/Apollo.ts
@@ -1,10 +1,23 @@
 import { ApolloLink } from 'apollo-link';
+import { createAuthLink } from 'aws-appsync-auth-link';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import appSyncConfig from 'aws-exports';
+import Amplify, { Auth } from 'aws-amplify';
+import { AUTH_TYPE } from 'aws-appsync';
+
+Amplify.configure(appSyncConfig);
 
 const url = appSyncConfig.aws_appsync_graphqlEndpoint;
+const region = (appSyncConfig as any).aws_appsync_region;
+
+const auth = {
+  type: appSyncConfig.aws_appsync_authenticationType as AUTH_TYPE,
+  jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+};
+
 const link = ApolloLink.from([
+  createAuthLink({ url, region, auth } as any) as any,
   createHttpLink({ uri: url }),
 ]);
 
