e64c16eae88bbda73995bff89f7219da4574f9d6
pkiop <pkiop@naver.com>
2021-04-05 22:46:44 +0900

    Feat: Get data from DB

--- client/src/page/survey/index.tsx
+++ client/src/page/survey/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { listQuestionnaire } from 'graphql/queries';
+import { listQuestionnaires } from 'graphql/queries';
 import Questionnaire from 'component/orgamisms/Questionnaire';
 import * as S from './style';
 
 function Survey() {
-  const questionList = ['React', 'Vue', 'Vanila Javascript', 'React', 'Vue', 'Vanila Javascript'];
-
   const {
     loading, error, data, refetch,
-  } = useQuery(gql`${listQuestionnaire}`);
+  } = useQuery(gql`${listQuestionnaires}`);
+
+  const questionnaireList = data.listQuestionnaires.items.map((el: any) => (
+    <Questionnaire
+      key={el.id}
+      question={el.questionTitle}
+      questionList={el.questionList}
+      bDuplicateSelect={el.bDuplicate} />
+  ));
 
   return (
     <S.SurveyPage>
-      <Questionnaire question={'선호하는 기술스택을 고르세요'} questionList={questionList} bDuplicateSelect={false} />
+      {questionnaireList}
     </S.SurveyPage>
   );
 }
