9532c27216ae82642a8e61bb726a021311aae5e4
pkiop <pkiop@naver.com>
2021-04-07 18:58:32 +0900

    Feat: Add pagecnt component

---
+++ client/src/component/molecules/PageCnt/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { arrowDirection } from 'component/atoms/ArrowBtn';
+import * as S from './style';
+
+interface IPageCnt {
+  currentPage: number;
+  totalPage: number
+  className?: string;
+}
+
+function PageCnt({ currentPage, totalPage, className }: IPageCnt) {
+  return (
+    <S.PageCnt className={className}>
+      <div> {currentPage} / {totalPage} </div>
+    </S.PageCnt>
+  );
+}
+
+export default PageCnt;
---
+++ client/src/component/molecules/PageCnt/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PageCnt = styled.div`
+  > div {
+    display: flex;
+    font-size: 4rem;
+  }
+`;
+
+export default PageCnt;
