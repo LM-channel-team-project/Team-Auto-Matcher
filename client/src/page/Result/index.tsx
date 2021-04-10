285be32121c6956e157d7b7199c79143c6488411
pkiop <pkiop@naver.com>
2021-04-10 09:44:30 +0900

    Feat: Add result page

---
+++ client/src/page/Result/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+function Result() {
+  return (
+    <S.ResultPage>
+      result page
+    </S.ResultPage>
+  );
+}
+
+export default Result;
---
+++ client/src/page/Result/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ResultPage = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const temp = styled.div``;
