30102f0dfafcc28946fd5e493d4f59f3bd7466d5
pkiop <pkiop@naver.com>
2021-05-05 15:05:02 +0900

    Feat: Add Team dashboard page

---
+++ client/src/page/Dashboard/Team/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './style';
+
+const TeamDashboardPage = ({ className }: any) => (
+  <S.TeamDashboardPage className={className}>
+    TEAM Dashboard
+  </S.TeamDashboardPage>
+);
+
+export default TeamDashboardPage;
---
+++ client/src/page/Dashboard/Team/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TeamDashboardPage = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default {};
