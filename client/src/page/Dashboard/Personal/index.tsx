2b3fcdcdab7928c77659d383f7722984aac712a6
pkiop <pkiop@naver.com>
2021-05-05 15:04:49 +0900

    Feat: Add Personal Dashboard page

---
+++ client/src/page/Dashboard/Personal/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './style';
+
+const PersonalDashboardPage = ({ className }: any) => (
+  <S.PersonalDashboardPage className={className}>
+    Personal Dashboard
+  </S.PersonalDashboardPage>
+);
+
+export default PersonalDashboardPage;
