b912d82271f4f09ce1ac43969232d64f771249de
unknown <kjcoco13@gmail.com>
2021-05-10 15:27:01 +0900

    change style of Login page

--- client/src/page/Login/index.tsx
+++ client/src/page/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import GoogleLogo from 'image/googlelogo.png';
+// import GoogleLogo from 'image/googlelogo.png';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import { Auth, Hub } from 'aws-amplify';
 import * as S from './style';
@@ -32,11 +32,16 @@ const LoginPage = ({ className }: any) => {
 
   return (
     <S.LoginPage className={className}>
-      <S.GoogleLogin
+      {/* <S.GoogleLogin
         icon={GoogleLogo}
         ButtonTitle={'Login With Google'}
         onClick={googleLoginOnClick}
-      />
+      /> */}
+      <S.Title>Team Auto Mathcer</S.Title>
+      <S.Button
+        onClick={googleLoginOnClick}>
+        구글 로그인
+      </S.Button>
     </S.LoginPage>
   );
 };
--- client/src/page/Login/style.ts
+++ client/src/page/Login/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import OAuthLoginComponent from 'component/molecules/OAuthLogin';
+// import OAuthLoginComponent from 'component/molecules/OAuthLogin';
 
 export const LoginPage = styled.div`
   height: 100%;
@@ -9,9 +9,24 @@ export const LoginPage = styled.div`
   align-items: center;
 `;
 
-export const GoogleLogin = styled(OAuthLoginComponent)`
-  border: 0.2rem solid black;
-  :hover {
-    cursor: pointer;
-  }
+export const Button = styled.button`
+  background-color: #696969;
+  border-radius: 10px;
+  width: 200px;
+  height: 50px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
 `;
+
+export const Title = styled.div`
+  font-size: 45px;
+  margin-bottom: 15%;
+`;
+
+// export const GoogleLogin = styled(OAuthLoginComponent)`
+//   border: 0.2rem solid black;
+//   :hover {
+//     cursor: pointer;
+//   }
+// `;
