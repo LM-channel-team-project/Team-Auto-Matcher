c2cf94de1317e38fbd1f6d98723d8415c9f8b334
YoungRong.Oh <youngrong.oh@gmail.com>
2021-05-20 22:44:27 +0900

    Revert "Revert "Feat: Add Detail Modal #24""
    This reverts commit 2e5603aefcbc24c578a5199c3e7811e749f5667f.

--- client/src/component/atoms/Button/index.stories.tsx
+++ client/src/component/atoms/Button/index.stories.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import Button from './index';
+import GlobalThemeProvider from 'style/GlobalThemeProvider';
+import Button, { Props } from './index';
 
 export default {
   title: 'Atoms/Button',
   component: Button,
 };
 
-export const HELLO = () => {
-  const subTitle = 'HELLO';
-  return <Button onClick={() => { alert(subTitle); }}>{subTitle}</Button>;
+const Template = (args: Props) => (
+  <GlobalThemeProvider>
+    <Button {...args} />
+  </GlobalThemeProvider>
+);
+
+export const Sizes = (props: Props) => {
+  const args = { onClick: (e: MouseEvent) => alert((e.target as HTMLElement)!.innerText) };
+
+  return (
+    <div>
+      <Template size='large' {...args} {...props} />
+      <Template size='medium' {...args} {...props} />
+      <Template size='small' {...args} {...props} />
+    </div>
+  );
 };
+
+export const AllButtons = () => (
+  <div style={{ margin: '2em' }}>
+    <Sizes children="Transparent"/>
+    <Sizes color='gray' children="Gray"/>
+    <Sizes color='red' children="Red"/>
+    <Sizes color='blue' children="Blue"/>
+    <Sizes color='yellow' children="Yellow"/>
+    <Sizes color='orange' children="Orange"/>
+  </div>
+);
---
+++ client/src/component/orgamisms/DetailModal/Team/index.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import GlobalThemeProvider from 'style/GlobalThemeProvider';
+import TeamDetailModal from '.';
+
+const props = {
+  data: {
+    id: 'aaaaa',
+    name: '라바마',
+    people: ['user1', 'user2', 'user3'],
+    skills: ['React', 'MongoDB', 'GraphQL'],
+    outline: '한줄 소개 입니다.',
+    contents: [
+      {
+        title: '구현하고자 하는 것',
+        text: '출시를 목적으로 하는 앱을 개발하려 합니다',
+      },
+      {
+        title: '진행 상황',
+        text: '아이디어를 내고 있는 중입니다.',
+      },
+      {
+        title: '스크롤 테스트',
+        text: `Ullamcorper convallis ante dignissim magna mi eleifend vestibulum elementum id nullam sem, 
+        ad per pretium lobortis sagittis fames porta odio iaculis. Rutrum porta nostra imperdiet potenti curae suscipit primis per platea lacus maecenas magnis, 
+        fames himenaeos nisi elementum condimentum proin posuere rhoncus fusce aenean. Convallis nascetur aptent fames volutpat lectus senectus leo elementum, purus tellus litora congue consequat tincidunt nostra maecenas, posuere feugiat nulla tempor habitant suspendisse ante. Bibendum fames phasellus parturient montes lectus sagittis, 
+        vulputate quisque facilisis lorem aenean felis nullam, sodales habitant nunc tempor etiam. Lacus imperdiet tellus commodo molestie luctus vitae euismod ad est cubilia lacinia sagittis, suscipit eleifend aliquet ornare fringilla consequat ridiculus sem justo conubia.`,
+      },
+    ],
+  },
+  onCloseModal: () => alert('close'),
+};
+
+export default {
+  title: 'Organisms/DetailModal/Team',
+  component: TeamDetailModal,
+};
+
+export function Default() {
+  return (
+    <GlobalThemeProvider>
+      <TeamDetailModal {...props} />
+    </GlobalThemeProvider>
+  );
+}
