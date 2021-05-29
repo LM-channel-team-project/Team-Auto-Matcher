0d008e9d7ba940df755dcd95970640419716f4c0
unknown <kjcoco13@gmail.com>
2021-05-29 22:09:50 +0900

    change px to rem

--- client/src/component/atoms/DirectionBtn/style.ts
+++ client/src/component/atoms/DirectionBtn/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Span = styled.span`
-    font-size: 28px;
+    font-size: 1.75rem;
     font-weight: 600;
     color: #160840;
     transition: all .1s;
