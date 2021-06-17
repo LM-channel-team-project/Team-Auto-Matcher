256487c51489e7108f678eac83cd9bbb18d393b3
unknown <kjcoco13@gmail.com>
2021-06-17 22:54:24 +0900

    Add home page

---
+++ client/src/page/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MenuBar from 'component/templates/menuBar';
+import * as S from './style';
+
+function Home() {
+  const sth = 0;
+  return (
+    <>
+      <MenuBar></MenuBar>
+      <S.Home>HomePage</S.Home>
+    </>
+  );
+}
+
+export default Home;
---
+++ client/src/page/Home/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Home = styled.div`
+    position: absolute;
+    top:50%;
+    left: 50%;
+`;
+
+export default Home;
