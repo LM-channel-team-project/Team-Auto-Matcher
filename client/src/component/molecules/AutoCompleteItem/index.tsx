99ac8bdf482606a40a675d97b336d2398a2dc305
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-23 22:58:02 +0900

    Feat: Add AutoCompleteItem

---
+++ client/src/component/molecules/AutoCompleteItem/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as S from './style';
+
+export interface Props extends S.StyleProps {
+  text: string;
+  divider?: boolean;
+  onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const AutoCompleteItem = ({
+  color, size = 'medium', text, onClick, ...props
+}: Props) => (
+  <S.AutoComplete {...props} onClick={onClick}>
+    <S.ColorCircle color={color} size={size} />
+    <S.Text size={size}>{text}</S.Text>
+  </S.AutoComplete>
+);
+
+export default AutoCompleteItem;
