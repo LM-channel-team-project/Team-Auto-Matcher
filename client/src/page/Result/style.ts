32f5171429a3274f5df335bcb4334219e6a24639
lhk3337 <lhk3337@naver.com>
2021-06-27 16:24:28 +0900

    result page 스타일링

--- client/src/page/Result/style.ts
+++ client/src/page/Result/style.ts
@@ -3,8 +3,6 @@ import QuestionResultComponent from 'component/orgamisms/QuestionResult';
 
 export const ResultPage = styled.div`
   display: flex;
-  /* width: 100%;
-  height: 100%; */
   flex-direction: column;
   justify-content: center;
   align-items: center;
