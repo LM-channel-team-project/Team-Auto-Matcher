3bc92bad4df262f04b761c33a9d400320d4fa4d2
unknown <kjcoco13@gmail.com>
2021-07-07 14:22:07 +0900

    Add datas

--- client/src/graphql/mutations.ts
+++ client/src/graphql/mutations.ts
@@ -2,12 +2,21 @@
 /* eslint-disable */
 
 export const createUser = /* GraphQL */ `
-  mutation createUser(
-      $input: CreateUserInput!
-    ) {
+  mutation createUser($input: CreateUserInput!) {
     createUser(input: $input) {
       id
-      userId 
+      userId
+      teamInfo {
+        name
+        skills
+        outline
+        state
+        contents {
+          title
+          text
+        }
+      }
+      surveyCompleted
       question {
         title
         answers
@@ -16,13 +25,22 @@ export const createUser = /* GraphQL */ `
   }
 `;
 
-export const updateUser= /* GraphQL */ `
-  mutation updateUser(
-      $input: UpdateUserInput!
-    ) {
+export const updateUser = /* GraphQL */ `
+  mutation updateUser($input: UpdateUserInput!) {
     updateUser(input: $input) {
       id
-      userId 
+      teamInfo {
+        name
+        skills
+        outline
+        state
+        contents {
+          title
+          text
+        }
+      }
+      surveyCompleted
+      userId
       question {
         title
         answers
@@ -32,19 +50,118 @@ export const updateUser= /* GraphQL */ `
 `;
 
 export const createTeam = /* GraphQL */ `
-  mutation createTeam(
-      $input: CreateTeamInput!
-    ) {
+  mutation createTeam($input: CreateTeamInput!) {
     createTeam(input: $input) {
       name
       people
       skills
       outline
+      owner
+      mail {
+        devExp
+        field
+        from
+        name
+        outline
+        skills
+        state
+        contents {
+          title
+          text
+        }
+      }
       contents {
         title
         text
       }
       state
+    }
+  }
+`;
+
+export const updateTeam = /* GraphQL */ `
+  mutation updateTeam($input: UpdateTeamInput!) {
+    updateTeam(input: $input) {
+      id
+      name
+      people
+      skills
+      outline
+      owner
+      mail {
+        devExp
+        field
+        from
+        name
+        outline
+        skills
+        state
+        contents {
+          title
+          text
+        }
+      }
+      contents {
+        title
+        text
+      }
+      state
+    }
+  }
+`;
+
+export const createPerson = /* GraphQL */ `
+  mutation createPerson($input: CreatePersonInput!) {
+    createPerson(input: $input) {
+      id
+      name
+      skills
+      outline
+      mail {
+        devExp
+        field
+        from
+        name
+        outline
+        skills
+        state
+        contents {
+          title
+          text
+        }
+      }
+      contents {
+        title
+        text
+      }
+    }
+  }
+`;
+
+export const updatePerson = /* GraphQL */ `
+  mutation updatePerson($input: UpdatePersonInput!) {
+    updatePerson(input: $input) {
+      id
+      name
+      skills
+      outline
+      mail {
+        devExp
+        field
+        from
+        name
+        outline
+        skills
+        state
+        contents {
+          title
+          text
+        }
+      }
+      contents {
+        title
+        text
+      }
     }
   }
 `;
