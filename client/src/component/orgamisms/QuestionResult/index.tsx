5757efe1f16e95cf62099dc543f363d36232f498
lhk3337 <lhk3337@naver.com>
2021-07-08 23:37:09 +0900

    result 페이지에서 personDashboard에 데이터 만들기 및 데이터 업데이트

--- client/src/component/orgamisms/QuestionResult/index.tsx
+++ client/src/component/orgamisms/QuestionResult/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { IAnswers } from 'component/molecules/QuestionRespond';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { createPerson, updateUser, updatePerson } from 'graphql/mutations';
+import { listPersonDashboard } from 'graphql/queries';
 import Button from 'component/atoms/Button';
 import * as S from './style';
 
 interface IQuestionResult {
   answerRespond: IAnswers[];
   className?: string;
+  id?: string;
+  surveyCompleted?: boolean;
 }
 
-function QuestionResult({ answerRespond, className }: IQuestionResult) {
+function QuestionResult({
+  answerRespond,
+  className,
+  id,
+  surveyCompleted,
+}: IQuestionResult) {
+  const { refetch } = useQuery(
+    gql`
+      ${listPersonDashboard}
+    `,
+  );
+  const [createPersonData] = useMutation(
+    gql`
+      ${createPerson}
+    `,
+  );
+  const [updateUserData] = useMutation(
+    gql`
+      ${updateUser}
+    `,
+  );
+  const [updatePersonData] = useMutation(
+    gql`
+      ${updatePerson}
+    `,
+  );
+
   const QuestionRespondList = answerRespond.map((answer: IAnswers) => (
     <S.QuestionRespond
       key={answer.title}
@@ -16,14 +47,85 @@ function QuestionResult({ answerRespond, className }: IQuestionResult) {
       answers={answer.answers}
     />
   ));
+
+  const onSubmit = async () => {
+    if (!surveyCompleted) {
+      await createPersonData({
+        variables: {
+          input: {
+            id,
+            name: answerRespond[11].answers[0],
+            field: answerRespond[0].answers[0],
+            skills: answerRespond[1].answers,
+            devExp: answerRespond[2].answers[0],
+            contents: {
+              title: answerRespond[8].title,
+              text: answerRespond[8].answers[0],
+            },
+            team: '팀 구하는중',
+            outline: answerRespond[10].answers[0],
+            periods: answerRespond[3].answers[0],
+            times: answerRespond[4].answers,
+            contact: answerRespond[5].answers[0],
+            hasCoWork: answerRespond[6].answers[0] === '경험 없음',
+            priority: answerRespond[7].answers,
+          },
+        },
+      });
+      updateUserData({
+        variables: {
+          input: {
+            id,
+            surveyCompleted: true,
+          },
+        },
+      });
+    } else {
+      updatePersonData({
+        variables: {
+          input: {
+            id,
+            name: answerRespond[11].answers[0],
+            field: answerRespond[0].answers[0],
+            skills: answerRespond[1].answers,
+            devExp: answerRespond[2].answers[0],
+            contents: {
+              title: answerRespond[8].title,
+              text: answerRespond[8].answers[0],
+            },
+            team: '팀 구하는중',
+            outline: answerRespond[10].answers[0],
+            periods: answerRespond[3].answers[0],
+            times: answerRespond[4].answers,
+            contact: answerRespond[5].answers[0],
+            hasCoWork: answerRespond[6].answers[0] === '경험 없음',
+            priority: answerRespond[7].answers,
+          },
+        },
+      });
+    }
+    refetch();
+    alert('확정 되었습니다.');
+    window.location.href = '/dashboard/personal';
+  };
+
   return (
     <>
       <S.QuestionResult className={className}>
         {QuestionRespondList}
       </S.QuestionResult>
-      <Button size="biglarge" color="gray">
-        확정
-      </Button>
+      <S.Btn>
+        <Button
+          className="confirm"
+          size="biglarge"
+          color="gray"
+          onClick={onSubmit}
+        >
+          {!surveyCompleted
+            ? '설문 결과 등록하기'
+            : '등록된 데이터 업데이트하기'}
+        </Button>
+      </S.Btn>
     </>
   );
 }
