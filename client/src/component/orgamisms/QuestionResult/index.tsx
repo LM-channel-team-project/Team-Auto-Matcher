89436fd247346a91aeb4c94921fe90e1d7852573
lhk3337 <lhk3337@naver.com>
2021-07-16 09:36:00 +0900

    updateUserData에 await 설정

--- client/src/component/orgamisms/QuestionResult/index.tsx
+++ client/src/component/orgamisms/QuestionResult/index.tsx
@@ -64,7 +64,7 @@ function QuestionResult({
       return;
     }
     if (!surveyCompleted) {
-      updateUserData({
+      await updateUserData({
         variables: {
           input: {
             id,
