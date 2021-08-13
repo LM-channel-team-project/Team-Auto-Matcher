9e5951246279beb7b037cd50ee00c6f6db995ee6
unknown <kjcoco13@gmail.com>
2021-08-13 17:52:58 +0900

    Add Notice path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import NotFound from 'page/NotFound';
 import Home from 'page/Home';
 import Contact from 'page/Contact';
 import Mail from 'page/Mail';
+import Notice from 'page/Notice';
 import { withAuthenticator } from 'aws-amplify-react';
 import { Auth } from 'aws-amplify';
 
@@ -24,26 +25,15 @@ function App() {
     <GlobalThemeProvider>
       <Router>
         <Switch>
-          <Route
-            exact path="/survey"
-            component={withAuthenticator(Survey, false, [<LoginPage />])}
-          />
-          <Route
-            exact path="/result"
-            component={withAuthenticator(Result, false, [<LoginPage />])}
-          />
+          <Route exact path="/survey" component={withAuthenticator(Survey, false, [<LoginPage />])}/>
+          <Route exact path="/result" component={withAuthenticator(Result, false, [<LoginPage />])}/>
           <Route render={(props) => <Home {...props} isLoggedIn={isLoggedIn}></Home>} exact path="/"/>
           <Route exact path="/contact" component={Contact} />
           <Route render={(props) => <TeamDashboard {...props} isLoggedIn={isLoggedIn}></TeamDashboard>} exact path="/dashboard/team"/>
-          <Route
-            exact path="/dashboard/personal"
-            component={PersonalDashboard}
-          />
+          <Route exact path="/dashboard/personal" component={PersonalDashboard}/>
           <Route exact path="/login" component={LoginPage} />
-          <Route
-            exact path="/mail"
-            component={withAuthenticator(Mail, false, [<LoginPage />])}
-          />
+          <Route exact path="/notice" component={Notice} />
+          <Route exact path="/mail" component={withAuthenticator(Mail, false, [<LoginPage />])}/>
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
