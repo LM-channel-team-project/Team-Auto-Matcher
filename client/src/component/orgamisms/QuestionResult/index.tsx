fed70937994eb83b10139eb5ed7e3bd40cecee01
Ho-s <kjcoco13@gmail.com>
2021-08-23 17:28:13 +0900

    set updateuserdata

--- client/src/component/orgamisms/QuestionResult/index.tsx
+++ client/src/component/orgamisms/QuestionResult/index.tsx
@@ -80,6 +80,7 @@ function QuestionResult({
         variables: {
           input: {
             id: userId,
+            question: answerRespond,
             surveyCompleted: true,
           },
         },
