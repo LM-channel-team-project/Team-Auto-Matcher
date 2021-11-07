e7b931c4e02c940c9aa7e38e81a246187c13b144
Ho-s <kjcoco13@gmail.com>
2021-11-07 20:02:39 +0900

    Remove conditional statement and add loading component before getting data in team add form  modal

--- client/src/component/orgamisms/DetailModal/TeamAddForm/index.tsx
+++ client/src/component/orgamisms/DetailModal/TeamAddForm/index.tsx
@@ -8,6 +8,7 @@ import makeTeamIdByUserId from 'utils/setTeamId';
 
 import ConfirmModal from 'component/orgamisms/ConfirmModal';
 import { Item } from 'component/orgamisms/AutoCompleteList';
+import LoadingPage from 'page/Loading';
 import { skillsLabel } from 'style/preset';
 import { TeamModalProps } from '../Team';
 import DetailModalTemplate, { ContentItem } from '../template';
@@ -79,7 +80,7 @@ const contentsTitle = [
 
 const TeamAddForm = ({ data, onCloseModal, onClickUpdate }: TeamModalProps) => {
   const history = useHistory();
-  const { data: userData, refetch } = useQuery(GET_USER);
+  const { data: userData, refetch, loading } = useQuery(GET_USER);
   const { refetch: teamRefetch } = useQuery(LIST_TEAM_DASHBOARD);
 
   const [createTeamData] = useMutation(CREATE_TEAM);
@@ -105,6 +106,10 @@ const TeamAddForm = ({ data, onCloseModal, onClickUpdate }: TeamModalProps) => {
   const completorRef = useRef<HTMLDivElement>();
   const [completor, setCompletor] = useState<Item[]>([]);
   const [focus, setFocus] = useState<number | null>(null);
+
+  if (loading) {
+    return <LoadingPage/>;
+  }
 
   const inputsState: InputsState = {
     name: {
@@ -356,90 +361,88 @@ const TeamAddForm = ({ data, onCloseModal, onClickUpdate }: TeamModalProps) => {
 
   const onMake = async () => {
     if (name.length < 2) return;
-    if (userData) {
-      if (!data) {
-        const userItems = userData.getUser.items[0];
-        if (!userItems.surveyCompleted) {
-          openModal();
-          setConfirmText(
-            '설문을 완료 후 팀을 만들어주세요. 확인을 누르면 설문 페이지로 넘어갑니다.',
-          );
-          setConfirmFunction(() => () => {
-            history.push('/survey');
-          });
-          return;
-        }
-        if (userItems.haveTeam) {
-          openModal();
-          setConfirmText('최대 한 개의 팀만 생성할 수 있습니다.');
-          const closeModals = () => {
-            onCloseModal();
-            closeModal();
-          };
-          setConfirmFunction(() => closeModals);
-          return;
-        }
-        const removeType = userItems.teamList.map((el: any) => ({
-          id: el.id,
-          name: el.name,
+    if (!data) {
+      const userItems = userData.getUser.items[0];
+      if (!userItems.surveyCompleted) {
+        openModal();
+        setConfirmText(
+          '설문을 완료 후 팀을 만들어주세요. 확인을 누르면 설문 페이지로 넘어갑니다.',
+        );
+        setConfirmFunction(() => () => {
+          history.push('/survey');
+        });
+        return;
+      }
+      if (userItems.haveTeam) {
+        openModal();
+        setConfirmText('최대 한 개의 팀만 생성할 수 있습니다.');
+        const closeModals = () => {
+          onCloseModal();
+          closeModal();
+        };
+        setConfirmFunction(() => closeModals);
+        return;
+      }
+      const removeType = userItems.teamList.map((el: any) => ({
+        id: el.id,
+        name: el.name,
+      }));
+      await createTeamData({
+        variables: {
+          input: {
+            id: makeTeamIdByUserId(userItems.id),
+            name,
+            people: [
+              { id: userItems.id, name: userItems.question[11].answers[0] },
+            ],
+            skills,
+            outline,
+            contents,
+            reponame,
+            owner: userItems.id,
+            state: '모집중',
+            createdAt: new Date(),
+            comments: [],
+          },
+        },
+      });
+      await teamRefetch();
+      await updateUserData({
+        variables: {
+          input: {
+            id: userItems.id,
+            haveTeam: true,
+            teamList: [...removeType, { id: makeTeamIdByUserId(userItems.id), name }],
+          },
+        },
+      });
+      await refetch();
+      onCloseModal();
+    } else {
+      const updateConfirm = async () => {
+        const removeType = contents.map((el: any) => ({
+          title: el.title,
+          text: el.text,
         }));
-        await createTeamData({
+        await updateTeamData({
           variables: {
             input: {
-              id: makeTeamIdByUserId(userItems.id),
+              id: data?.id,
               name,
-              people: [
-                { id: userItems.id, name: userItems.question[11].answers[0] },
-              ],
               skills,
               outline,
-              contents,
               reponame,
-              owner: userItems.id,
-              state: '모집중',
-              createdAt: new Date(),
-              comments: [],
+              contents: removeType,
+              state: teamState,
             },
           },
         });
         await teamRefetch();
-        await updateUserData({
-          variables: {
-            input: {
-              id: userItems.id,
-              haveTeam: true,
-              teamList: [...removeType, { id: makeTeamIdByUserId(userItems.id), name }],
-            },
-          },
-        });
-        await refetch();
-        onCloseModal();
-      } else {
-        const updateConfirm = async () => {
-          const removeType = contents.map((el: any) => ({
-            title: el.title,
-            text: el.text,
-          }));
-          await updateTeamData({
-            variables: {
-              input: {
-                id: data?.id,
-                name,
-                skills,
-                outline,
-                reponame,
-                contents: removeType,
-                state: teamState,
-              },
-            },
-          });
-          await teamRefetch();
-          onClickUpdate();
-        };
-        openModal();
-        setConfirmText('확인을 누르면 업데이트가 완료됩니다.');
-        setConfirmFunction(() => updateConfirm);
-      }
+        onClickUpdate();
+      };
+      openModal();
+      setConfirmText('확인을 누르면 업데이트가 완료됩니다.');
+      setConfirmFunction(() => updateConfirm);
     }
   };
 
